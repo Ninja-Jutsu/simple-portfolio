1216e39c8f631b483af71afc96a43c6626f5fdde
ismail-bardach <ismail.bardach.work@gmail.com>
2024-08-02 14:04:43 +0800

    Edit Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,31 +1,31 @@
 // TODO: When the user clicks the SmallLogo, scroll back to the top
 
-import React, { ReactElement } from 'react';
-import styles from '../styles/components/Footer.module.scss';
-import SmallLogo from './SmallLogo';
+import React, { ReactElement } from 'react'
+import styles from '../styles/components/Footer.module.scss'
+import SmallLogo from './SmallLogo'
 
 interface Props {
-  heroRef: any;
+  heroRef: any
 }
 
 function Footer({ heroRef }: Props): ReactElement {
   return (
     <footer className={styles.container}>
       <div className={styles.content}>
-        <span>Pedro Klepa &middot; 2021</span>
+        <span>Ismail Bardach &middot; 2024</span>
         <SmallLogo
           onClick={() => {
             heroRef.current.scrollIntoView({
               behavior: 'smooth',
               block: 'nearest',
               inline: 'start',
-            });
+            })
           }}
         />
         <span>All rights reserved.</span>
       </div>
     </footer>
-  );
+  )
 }
 
-export default Footer;
+export default Footer
